75961f28ffb53aba83aa37d8a447e8656cb950ca
Daniel Osorio torres <131273609+dani101030@users.noreply.github.com>
2024-04-02 13:37:29 -0500

    cambios

--- servidor.py
+++ servidor.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
-import graphviz 
-import os
+from direct_dfa import DDFA
+from reader import Reader
+from parsing import Parser
+from nfa import NFA
+from dfa import DFA
+
 
 app = Flask(__name__, static_folder='static')
 app.static_folder = 'static'
@@ -14,62 +18,33 @@ def home():
         regex = request.form['regex']
         print(f"Expresión regular recibida: {regex}")
         try:
-            nombre_imagen = graficar_regex(regex)
-            return render_template('graph.html', regex=regex, imagen=nombre_imagen)
+            automaton_type = request.form['automaton_type']
+            reader = Reader(regex)
+            tokens = reader.CreateTokens()
+            parser = Parser(tokens)
+            tree = parser.Parse()
+            if automaton_type == "DirectDFA":
+                ddfa = DDFA(tree, reader.GetSymbols(), regex)
+                ddfa.GraphDFA()
+
+                return render_template('graph.html', regex=regex, imagen="grafo.png")
+            elif automaton_type == "NFA":
+                nfa = NFA(tree, reader.GetSymbols(), regex)
+                nfa.WriteNFADiagram()
+                return render_template('graph.html', regex=regex, imagen="grafo.png")
+            elif automaton_type == "DFA":
+                nfa = NFA(tree, reader.GetSymbols(), regex)
+                dfa = DFA(nfa.trans_func, nfa.symbols,nfa.curr_state, nfa.accepting_states, regex)
+                dfa.TransformNFAToDFA()
+                dfa.GraphDFA()
+                return render_template('graph.html', regex=regex, imagen="grafo.png")
+            else:
+                print("¡Opción no válida! Por favor, ingrese 'NFA', 'DFA' o 'DirectDFA'.")
+                return render_template('index.html')
         except Exception as e:
-            print(f"Error al graficar la expresión regular: {e}")
+            print(f"Error: {e}")
             return render_template('graph.html', regex=regex, imagen="")
     return render_template('index.html')
 
-
-@app.route('/graph')
-def graph():
-    print("Entrando a la ruta /graph")  # Agregar esta línea
-    regex = request.args.get('regex')
-    print(f"Expresión regular recibida: {regex}")  # Agregar esta línea
-    nombre_imagen = graficar_regex(regex)
-    return render_template('graph.html', regex=regex, imagen=nombre_imagen)
-
-def graficar_regex(regex):
-    print("Entrando a la función graficar_regex")  # Agregar esta línea
-
-    # Crear un nuevo grafo
-    grafo = graphviz.Digraph()
-
-    # Configurar el diseño del grafo
-    grafo.graph_attr['rankdir'] = 'LR'
-    grafo.node_attr['shape'] = 'circle'
-
-    # Crear los nodos y las aristas según la expresión regular
-    estado_actual = 0
-    for c in regex:
-        if c == '(':
-            estado_actual += 1
-            grafo.node(str(estado_actual))
-        elif c == ')':
-            grafo.edge(str(estado_actual), str(estado_actual - 1))
-        elif c == '+':
-            estado_actual += 1
-            grafo.node(str(estado_actual))
-            grafo.edge(str(estado_actual - 1), str(estado_actual), label='+')
-            grafo.edge(str(estado_actual), str(estado_actual - 1), label='+')
-        elif c == '*':
-            grafo.edge(str(estado_actual), str(estado_actual), label='*')
-        else:
-            estado_actual += 1
-            grafo.node(str(estado_actual))
-            grafo.edge(str(estado_actual - 1), str(estado_actual), label=c)
-
-    # Generar la imagen en formato PNG
-    nombre_imagen = 'grafo'
-    ruta_imagen = os.path.join('static', nombre_imagen)
-    print(f"Generando la imagen en: {ruta_imagen}")
-    grafo.render(ruta_imagen, view=True, format='png', cleanup=True)
-
-
-    return nombre_imagen
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
